8f8bc98ec545b9f54b51a7955822776eee27a108
Saman Mahdanian <saman@mahdanian.xyz>
2021-08-04 11:41:17 +0430

    LCA bug fixed

--- LCA.cpp
+++ LCA.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+const int N = 1E6 + 10;
+const int L = 25;
+
+int n, h[N], par[N][L];
+vector <int> g[N];
+
 void inputGraph();								// inputs the Graph
 int LCA(int, int);								// returns the LCA of two vertex
 void preProccess(int = 0, int = 0, int = 0);	// resolves parents and heights
@@ -39,7 +45,8 @@ void preProccess(int v, int pv, int cur_h) {
 		par[v][l] = par[par[v][l - 1]][l - 1];
 
 	for (int u: g[v])
-		preProccess(u, v, cur_h + 1);
+		if (u != pv)
+			preProccess(u, v, cur_h + 1);
 }
 
 int LCA(int v, int u) {
@@ -47,7 +54,7 @@ int LCA(int v, int u) {
 		swap(v, u);
 
 	for (int l = L - 1; l >= 0; l--)
-		if (h[par[v][l]] <= h[u])
+		if (h[par[v][l]] >= h[u])
 			v = par[v][l];
 	if (v == u)
 		return v;

cec6de34f3f40fe74851bfc57765236e2322dcfd
Saman Mahdanian <saman@mahdanian.xyz>
2021-08-11 11:39:43 +0430

    MAP added

---
+++ MAP.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+map<int, string> m;
+set< pair<int, string> > maps;
+
+void add_map(int, string);
+void erase_map(int);
+string get_map(int);
+
+// (int) ---> (string)
+
+int main() {
+	// Mapping using SET
+	add_map(12, "salam");
+	add_map(13, "khodafez");
+	add_map(12, "hi");
+	erase_map(13);
+	cout << get_map(12) << endl;
+
+	// Mapping using Array
+	string s[1000 * 1000] = {};
+	s[12] = "salam";
+	s[13] = "khodafez";
+	s[12] = "hi";
+	s[13] = "";
+	cout << s[12] << endl;
+
+	// Mapping using std::map
+	m[12] = "salam";
+	m[13] = "khodafez";
+	m[12] = "hi";
+	m.erase(13);
+	cout << m[12] << endl;
+
+	// Also about std::map
+	m.empty();
+	m.size();
+	m.clear();
+
+	map<string, int> score;
+	t["bahar"] = 10;
+	t["roshan"] = 9;
+	t["armita"] = 11;
+
+	map<long long, int> compressor; // !!
+}
+
+void add_map(int v, string s) {
+	erase_map(v);
+	maps.insert({v, s});
+}
+
+void erase_map(int v) {
+	auto it = maps.lower_bound({v, ""});
+	if (it == maps.end() || (*it).first != v)
+		return;
+	maps.erase(it);
+}
+
+string get_map(int v) {
+	auto it = maps.lower_bound({v, ""});
+	if (it == maps.end() || (*it).first != v)
+		return "";
+	return (*it).second;
+}
